c831ae7c213339f65749c0635b60f948c3d6eefc
Brandon Reyes <brandon.reyesmi@gmail.com>
2022-01-15 19:46:20 -0600

    feat: add custom components in chat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './Components/ChatFeed';
 
 const App = () => {
     return (
         <ChatEngine 
             height="100vh"
-            projectID=""
-            userName=""
-            userSecret=""
+            projectID={process.env.REACT_APP_PROJECT_ID}
+            userName={process.env.REACT_APP_USER_NAME}
+            userSecret={process.env.REACT_APP_PASSWORD}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
 }
